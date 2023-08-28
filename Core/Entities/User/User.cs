0d4aa5e061b2d881580c1c0f1d130f8ff47b4579
Mateusz <37417315+Mbialas1@users.noreply.github.com>
2023-08-28 21:43:52 +0200

    Set main struct of core domain

---
+++ Core/Entities/User/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.User
+{
+    internal class User
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+        public string Salt { get; set; }
+        public int FailedLoginAttempts { get; set; }
+        public DateTime LastLogin { get; set; }
+        public DateTime? LockoutEnd { get; set; }
+    }
+}
