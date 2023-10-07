c57770b1ada19ddcc18b90d95a676cde5c19f3c6
Mbialas <mbialaas@gmail.com>
2023-10-07 20:47:05 +0200

    Adding good practics for microservices. Add polling

--- Core.Application/Services/UserCommunicationService .cs
+++ Core.Application/Services/UserCommunicationService .cs
@@ -1,7 +1,9 @@
 ﻿using Core.Services.InterfaceServices;
+using Polly;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,16 +13,24 @@ namespace Core.Application.Services
     {
         private readonly HttpClient httpClient;
         private const string URL_GET_USER = "/Users/users/";
+
+        #region Constructor
         public UserCommunicationService(HttpClient _httpClient)
         {
             httpClient = _httpClient;
         }
+        #endregion
+
+        private static readonly IAsyncPolicy<HttpResponseMessage> _circuitBreakerPolicy = Policy
+        .Handle<HttpRequestException>()
+        .OrResult<HttpResponseMessage>(response => !response.IsSuccessStatusCode)
+        .CircuitBreakerAsync(5, TimeSpan.FromMinutes(2));
 
         public async Task<bool> UserExists(long userId)
         {
             try
             {
-                HttpResponseMessage response = await httpClient.GetAsync($"{URL_GET_USER}{userId}");
+                HttpResponseMessage response = await _circuitBreakerPolicy.ExecuteAsync(() =>  httpClient.GetAsync($"{URL_GET_USER}{userId}"));
                 return response.IsSuccessStatusCode;
 
             }
